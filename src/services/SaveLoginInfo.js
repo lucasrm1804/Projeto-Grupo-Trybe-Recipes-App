26ff1e183c970bd900139a15dc98bf62e1ea365a
furquin <laert.ff@gmail.com>
2022-01-28 19:56:08 -0300

    feat: implementa funçoes para salvar no localStorage

---
+++ src/services/SaveLoginInfo.js
@@ -0,0 +1,5 @@
+export const saveUserInfos = (info) => localStorage.setItem('user', info);
+
+export const saveMealsToken = () => localStorage.setItem('mealsToken', 1);
+
+export const savecocktailsToken = () => localStorage.setItem('cocktailsToken', 1);
