204d261bfc520f9802ea52b81fa7c5abc1077949
furquin <laert.ff@gmail.com>
2022-01-28 17:58:22 -0300

    feat: rendeza page receitas no App , para facilitar implementaçao

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer';
+import PrincipalReceitas from './Pages/PrincipalReceitas';
 
 function App() {
   return (
     <div>
       <Footer />
+      <PrincipalReceitas />
     </div>
   );
 }
