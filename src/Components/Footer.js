cacc8cb1a0f771f532a706f0760075c4803faa80
furquin <laert.ff@gmail.com>
2022-01-28 16:39:39 -0300

    feat : implementa logica para redirecionar usuario

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import '../App.css';
 
 export default function Footer() {
-  return (
-    <div data-testid="footer">
+  const [redirect, setRedirect] = useState(false);
+  const [OnRoute, setOnRoute] = useState('');
+  const [routes] = useState({ drink: '/drink', food: '/food', explore: '/explore' });
 
+  const onClick = (value) => {
+    setRedirect(true);
+    setOnRoute(value);
+  };
+
+  return (
+
+    <div
+      className="footer"
+      data-testid="footer"
+    >
       <button
         type="button"
         data-testid="drinks-bottom-btn"
+        onClick={ () => onClick(routes.drink) }
       >
         <img src="src/images/drinkIcon.svg" alt="drinkIcon" />
       </button>
@@ -14,15 +29,19 @@ export default function Footer() {
       <button
         type="button"
         data-testid="explore-bottom-btn"
+        onClick={ () => onClick(routes.explore) }
       >
         <img src="src/images/exploreIcon.svg" alt="exploreIcon" />
+
       </button>
 
       <button
         type="button"
         data-testid="food-bottom-btn"
+        onClick={ () => onClick(routes.food) }
       >
         <img src="src/images/meals.svg" alt="mealsIcon" />
       </button>
+      { redirect && <Redirect to={ OnRoute } /> }
     </div>);
 }
