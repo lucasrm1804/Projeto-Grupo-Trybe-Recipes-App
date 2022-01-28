bae7380a2a03fe1975c5751662a90f46273c103d
furquin <laert.ff@gmail.com>
2022-01-28 19:51:33 -0300

    fix: exporto componete footer para pasta ja existente

---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function LoginButton(props) {
+  const { label, testId, handleClick, isDisabled } = props;
+  return (
+    <button
+      data-testid={ testId }
+      type="button"
+      onClick={ () => handleClick() }
+      disabled={ isDisabled }
+      className={ isDisabled
+        ? `bg-blue-500 text-white font-bold w-28 h-12 rounded opacity-50
+        cursor-not-allowed`
+        : 'bg-blue-500 hover:bg-blue-700 text-white font-bold w-28 h-12 rounded' }
+    >
+      { label }
+    </button>
+  );
+}
+
+LoginButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  testId: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  isDisabled: PropTypes.bool.isRequired,
+};
+
+export default LoginButton;
