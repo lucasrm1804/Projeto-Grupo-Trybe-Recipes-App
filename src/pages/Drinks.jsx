77b54c89cfc22b90d99d5382bdabc7d86126236d
furquin <laert.ff@gmail.com>
2022-02-03 15:24:48 -0300

    feat: implementa botao de pesquisa

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as COMP from '../components';
+import HeaderContext from '../context/Header/HeaderContext';
 
 export default function Drinks() {
+  const { displaySearchBar } = useContext(HeaderContext);
   return (
     <div>
       <div>
         <COMP.Header showSearch label="Drinks" />
       </div>
+
+      <div>
+        {displaySearchBar && <COMP.SearchBar title="Drinks" />}
+
+      </div>
       <div className="flex justify-center">
-        <div className=" fixed bottom-3 w-full flex max-w-sm ">
+        <div className=" fixed bottom-0 w-full flex max-w-sm items-end ">
           <COMP.Footer />
         </div>
       </div>
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as COMP from '../components';
+import HeaderContext from '../context/Header/HeaderContext';
 
 export default function Foods() {
+  const { displaySearchBar } = useContext(HeaderContext);
   return (
     <div>
       <div>
         <COMP.Header showSearch label="Foods" />
+      </div>
+
+      <div>
+        {displaySearchBar && <COMP.SearchBar title="Foods" />}
+
       </div>
       <div className="flex justify-center">
         <div className=" fixed bottom-0 w-full flex max-w-sm items-end ">
