ada83b01bafea5adfa08bc9e09ce950e5b8e2182
furquin <laert.ff@gmail.com>
2022-02-03 16:23:34 -0300

    feat: implementa logica para renderizar cads

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,19 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import * as COMP from '../components';
 import HeaderContext from '../context/Header/HeaderContext';
+import DrinksAndFoodsContext from '../context/DrinksAndFoods/DrinksAndFoodsContext';
+import { drinkApiDidMount } from '../services/ApiDrinks';
+import DrinkCards from '../components/DrinkCards';
 
 export default function Drinks() {
   const { displaySearchBar } = useContext(HeaderContext);
+  const { drinks, setDrinks } = useContext(DrinksAndFoodsContext);
+  const TWELVE = 12;
+
+  useEffect(() => {
+    drinkApiDidMount(setDrinks);
+  }, []);
   return (
     <div>
       <div>
         <COMP.Header showSearch label="Drinks" />
       </div>
-
       <div>
         {displaySearchBar && <COMP.SearchBar title="Drinks" />}
 
       </div>
+      <div>
+        {drinks && drinks.slice(0, TWELVE).map((drink, index) => (
+          <DrinkCards
+            key={ drink.idDrink }
+            index={ index }
+            idDrink={ drink.idDrink }
+            strDrink={ drink.strDrink }
+            strDrinkThumb={ drink.strDrinkThumb }
+
+          />
+        ))}
+      </div>
+
       <div className="flex justify-center">
         <div className=" fixed bottom-0 w-full flex max-w-sm items-end ">
           <COMP.Footer />
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,19 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import * as COMP from '../components';
 import HeaderContext from '../context/Header/HeaderContext';
+import DrinksAndFoodsContext from '../context/DrinksAndFoods/DrinksAndFoodsContext';
+import { apiMealsDidMount } from '../services/ApiFood';
+import FoodCards from '../components/FoodCards';
 
 export default function Foods() {
   const { displaySearchBar } = useContext(HeaderContext);
+  const { meals, setMeals } = useContext(DrinksAndFoodsContext);
+  const TWELVE = 12;
+
+  useEffect(() => {
+    apiMealsDidMount(setMeals);
+  }, []);
   return (
     <div>
       <div>
         <COMP.Header showSearch label="Foods" />
       </div>
-
       <div>
         {displaySearchBar && <COMP.SearchBar title="Foods" />}
 
       </div>
+      <div>
+        {meals && meals.slice(0, TWELVE).map((food, index) => (
+          <FoodCards
+            key={ food.idMeal }
+            index={ index }
+            idMeal={ food.idMeal }
+            strMeal={ food.strMeal }
+            strMealThumb={ food.strMealThumb }
+          />
+        ))}
+      </div>
+
       <div className="flex justify-center">
         <div className=" fixed bottom-0 w-full flex max-w-sm items-end ">
           <COMP.Footer />
