b21cbfb6f68100b283fabd33c351d852b075c809
furquin <laert.ff@gmail.com>
2022-02-05 00:38:56 -0300

    feat: inicia logica para paginas de receitas em progresso '

--- src/pages/FoodsInProgress.jsx
+++ src/pages/FoodsInProgress.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import FoodsInProgressHeader from '../components/RecipesInProgress/FoodsInProgressHeader';
+import ReceitaAtualContext from '../context/ReceitaAtual/ReceitaAtualContext';
+import { apiReceitaAtual } from '../services/ApiFood';
 
 export default function FoodsInProgress() {
-  return <div />;
+  const { receita, setReceita } = useContext(ReceitaAtualContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    apiReceitaAtual(id, setReceita);
+  }, [id, setReceita]);
+
+  const { strMeal, strMealThumb } = receita;
+  return (
+    <div>
+      <FoodsInProgressHeader
+        strMeal={ strMeal }
+        strMealThumb={ strMealThumb }
+      />
+    </div>
+  );
 }
