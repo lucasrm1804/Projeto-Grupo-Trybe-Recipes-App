a165b7857c7a7a1f8ceb67388c203b5ebac60333
furquin <laert.ff@gmail.com>
2022-02-10 19:11:20 -0300

    fix: implementa logica para mensagem de copiada

--- src/components/ButtonShare/ButtonShare.jsx
+++ src/components/ButtonShare/ButtonShare.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import shareIcon from '../../images/shareIcon.svg';
 
 export default function ButtonShare() {
+  const [copied, setCopied] = useState(false);
   const onClickCopied = () => {
     const url = window.location.href;
     navigator.clipboard.writeText(url.replace('/in-progress', ''));
+    setCopied(!copied);
   };
 
   return (
-    <button
-      className="px-3 py-2"
-      data-testid="share-btn"
-      type="button"
-      onClick={ () => {
-        onClickCopied();
-        alert('Link copied!');
-      } }
-    >
-      <img
-        src={ shareIcon }
-        alt="Share Icon"
-      />
-    </button>
+    <div>
+      <button
+        className="px-3 py-2"
+        data-testid="share-btn"
+        type="button"
+        onClick={ onClickCopied }
+      >
+        <img
+          src={ shareIcon }
+          alt="Share Icon"
+        />
+      </button>
+      {copied && <p>Link copied!</p>}
+    </div>
   );
 }
