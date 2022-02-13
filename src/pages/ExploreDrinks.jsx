998ba531037cd14d44440c72f043ed711fde9e0b
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-02-13 16:14:13 -0300

    Implementa pagina de explorar - Finalizada

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import * as COMP from '../components';
+import { randomDrinks } from '../services/ApiDrinks';
 
-export default function ExploreIngredientsDrinks() {
+export default function ExploreDrinks() {
+  const history = useHistory();
+  const [randomId, setRandomId] = useState('');
+
+  useEffect(() => {
+    randomDrinks(setRandomId);
+  }, []);
   return (
     <div className="w-full h-auto mt-20">
       <div>
         <COMP.Header label="Explore Drinks" />
+      </div>
+      <div>
+        <button
+          data-testid="explore-by-ingredient"
+          type="button"
+          onClick={ () => history.push('/explore/drinks/ingredients') }
+        >
+          By Ingredient
+        </button>
+      </div>
+      <div>
+        <button
+          data-testid="explore-surprise"
+          type="button"
+          onClick={ () => history.push(`/drinks/${randomId}`) }
+        >
+          Surprise me!
+        </button>
       </div>
       <div className="flex justify-center">
         <div className=" fixed bottom-0 w-full flex max-w-sm ">
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import * as COMP from '../components';
+import { randomFoods } from '../services/ApiFood';
 
-export default function ExploreIngredientsDrinks() {
+export default function ExploreFoods() {
+  const history = useHistory();
+  const [randomId, setRandomId] = useState('');
+
+  useEffect(() => {
+    randomFoods(setRandomId);
+  }, []);
+
   return (
     <div className="w-full h-auto mt-20">
       <div>
         <COMP.Header label="Explore Foods" />
+      </div>
+      <div>
+        <button
+          data-testid="explore-by-ingredient"
+          type="button"
+          onClick={ () => history.push('/explore/foods/ingredients') }
+        >
+          By Ingredient
+        </button>
+      </div>
+      <div>
+        <button
+          data-testid="explore-by-nationality"
+          type="button"
+          onClick={ () => history.push('/explore/foods/nationalities') }
+        >
+          By Nationality
+        </button>
+      </div>
+      <div>
+        <button
+          data-testid="explore-surprise"
+          type="button"
+          onClick={ () => history.push(`/foods/${randomId}`) }
+        >
+          Surprise me!
+        </button>
       </div>
       <div className="flex justify-center">
         <div className=" fixed bottom-0 w-full flex max-w-sm ">
